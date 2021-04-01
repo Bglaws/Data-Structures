da15b4cf5e5af8a32522676b8c2904c13d1454bf
Brian Glaws <glaws.brian@gmail.com>
2021-04-01 15:05:13 -0400

    Templating progress

--- src/DList.java
+++ src/DList.java
@@ -1,10 +1,102 @@
 package src;
 
-import java.util.Iterator;
+import java.util.*;
+import java.io.Serializable;
+import java.lang.reflect.Array;
 
-public class DList<E> implements Iterable<E> {
+public class DList<E> 
+
+    implements Iterable, List<E>, Deque<E>, Cloneable, Serializable {
 
     private DListNode<E> nil;
+
+    private static class DListNode<T> {
+        public T data;
+        public DListNode next;
+        public DListNode previous;
+    }
+
+    private class DListIterator implements Iterator<E> {
+        private DListNode pointer;
+
+        public DListIterator() {
+            if (nil.next == nil)
+                pointer = nil;
+            else
+                pointer = nil.next;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (this.pointer == nil) {
+                return false;
+            } 
+            return true;
+        }
+
+        @Override
+        public E next() {
+            if(!hasNext()){
+                return null;
+            }
+            DListNode temp = this.pointer;
+            this.pointer = this.pointer.next;
+            return (E) temp.data;
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        DList a = new DList();
+        DList b = new DList();
+
+        String elem1 = "P";
+        String elem2 = "O";
+        String elem3 = "P";
+        String elem4 = "C";
+        String elem5 = "O";
+        String elem6 = "R";
+        String elem7 = "N";
+
+        a.addFirst(elem1);
+        a.addFirst(elem2);
+        a.addFirst(elem3);
+        a.addLast(elem4);
+        a.addLast(elem5);
+        a.addLast(elem6);
+        a.addLast(elem7);
+        
+        Iterator<String> it = a.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        System.out.println();
+
+        a.removeFirst();
+        a.removeFirst();
+        a.set(0, "S");        
+
+        it = a.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        System.out.println();
+
+        System.out.println(a.get(a.size() - 1) + "\n");
+        a.removeLast();
+        a.addLast("E");
+
+        it = a.iterator();
+        while (it.hasNext()) {
+            System.out.println(it.next());
+        }
+        System.out.println();
+
+        System.out.println(a.contains("O"));
+        System.out.println(a.indexOf("S"));
+        
+    }
 
     public DList() {
         nil = new DListNode();
@@ -133,92 +225,6 @@ public class DList<E> implements Iterable<E> {
         return new DListIterator();
     }
 
-    private static class DListNode<T> {
-        public T data;
-        public DListNode next;
-        public DListNode previous;
-    }
-
-    private class DListIterator implements Iterator<E> {
-        private DListNode pointer;
-
-        public DListIterator() {
-            if (nil.next == nil)
-                pointer = nil;
-            else
-                pointer = nil.next;
-        }
-
-        @Override
-        public boolean hasNext() {
-            if (this.pointer == nil) {
-                return false;
-            } 
-            return true;
-        }
-
-        @Override
-        public E next() {
-            if(!hasNext()){
-                return null;
-            }
-            DListNode temp = this.pointer;
-            this.pointer = this.pointer.next;
-            return (E) temp.data;
-        }
-
-    }
-
-    public static void main(String[] args) {
-
-        DList a = new DList();
-        DList b = new DList();
-
-        String elem1 = "P";
-        String elem2 = "O";
-        String elem3 = "P";
-        String elem4 = "C";
-        String elem5 = "O";
-        String elem6 = "R";
-        String elem7 = "N";
-
-        a.addFirst(elem1);
-        a.addFirst(elem2);
-        a.addFirst(elem3);
-        a.addLast(elem4);
-        a.addLast(elem5);
-        a.addLast(elem6);
-        a.addLast(elem7);
-        
-        Iterator<String> it = a.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
-        System.out.println();
-
-        a.removeFirst();
-        a.removeFirst();
-        a.set(0, "S");        
-
-        it = a.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
-        System.out.println();
-
-        System.out.println(a.get(a.size() - 1) + "\n");
-        a.removeLast();
-        a.addLast("E");
-
-        it = a.iterator();
-        while (it.hasNext()) {
-            System.out.println(it.next());
-        }
-        System.out.println();
-
-        System.out.println(a.contains("O"));
-        System.out.println(a.indexOf("S"));
-        
-    }
+   
 
 }

662d3af2f250198c25ea9da19e568f78debaf339
Bglaws <glaws.brian@gmail.com>
2021-05-03 22:47:03 -0400

    Changed checkoutLine in HardwarStore from ArrayList to PQ

--- src/Homework4/HardwareStore.java
+++ src/Homework4/HardwareStore.java
@@ -1,15 +1,17 @@
 package Homework4;
 
 import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class HardwareStore {
-    private ArrayList<Queue<Shopper>> store;
+    private PriorityQueue<Queue<Shopper>> checkoutLines;
     private int totalRevenue;
     private LinkedList<Shopper> shortestLine;
-    private PriorityQueue<Shopper> pq;
+    private PriorityQueue<Shopper> store;
 
     /*
      * HardwareStore Constructor. This constructor instantiates the "store"
@@ -18,12 +20,14 @@ public class HardwareStore {
      * structure.
      */
     HardwareStore(int numRegisters) {
-        this.store = new ArrayList<Queue<Shopper>>();
-        this.pq = new PriorityQueue<Shopper>();
+        this.checkoutLines = new PriorityQueue<Queue<Shopper>>((Queue<Shopper> q1, Queue<Shopper> q2) -> {
+            return (q1.size() < q2.size());
+        });
+        this.store = new PriorityQueue<Shopper>();
         this.totalRevenue = 0;
         this.shortestLine = null;
         while (numRegisters > 0) {
-            store.add(new LinkedList<Shopper>());
+            checkoutLines.add(new LinkedList<Shopper>());
             numRegisters--;
         }
     }
@@ -31,11 +35,18 @@ public class HardwareStore {
     // HERE! not sure if adding shopper to priority que is this simple?
     // need to research priorityQueue.add
     void addShopperToLine(Shopper shopper) {
-        pq.add(shopper);
+        store.add(shopper);
     }
 
     void checkoutAllShoppers() {
+        // size is used to save the size of the shortest checkout line.
+        int size = 0;
+        Queue<Shopper> temp;
 
+        while (!store.isEmpty()) {
+            temp = checkoutLines.poll();
+            temp.offer(store.poll());
+        }
     }
 
     double totalRevenue() {
